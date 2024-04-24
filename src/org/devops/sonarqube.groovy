d7567f1feed55980401d5a2a07749f29289fce14
roger20191106 <79368866+roger20191106@users.noreply.github.com>
2024-04-24 18:36:21 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -21,8 +21,8 @@ def SonarScan(sonarServer,projectName,projectDesc,projectPath,branchName){
             -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 \
             -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.baidu.com \
             -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
-            -Dsonar.login=admin -Dsonar.password=admin123
-            -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports -X
+            -Dsonar.login=admin -Dsonar.password=admin123 \
+            -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports -Dsonar.branch.name=${branchName} -X
 
         """)
         
@@ -31,7 +31,8 @@ def SonarScan(sonarServer,projectName,projectDesc,projectPath,branchName){
             -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 \
             -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.baidu.com \
             -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
-            -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports  -Dsonar.branch.name=${branchName} -X
+            -Dsonar.login=admin -Dsonar.password=admin123 \
+            -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports -X
 
         """
 //    }
