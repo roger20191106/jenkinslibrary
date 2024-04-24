bc0725cc275458b5c0d3c1549228f43b19336bf4
roger20191106 <79368866+roger20191106@users.noreply.github.com>
2024-04-24 21:27:31 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -36,10 +36,10 @@ def SonarScan(sonarServer,projectName,projectDesc,projectPath,branchName){
         """
     }
     
-    def qg = waitForQualityGate()
-    if (qg.status != 'OK') {
-        error "Pipeline aborted due to quality gate failure: ${qg.status}"
-    } else {
-        println("Quality pass!")
-    }
+    //def qg = waitForQualityGate()
+    //if (qg.status != 'OK') {
+    //    error "Pipeline aborted due to quality gate failure: ${qg.status}"
+    //} else {
+    //    println("Quality pass!")
+    //}
 }
